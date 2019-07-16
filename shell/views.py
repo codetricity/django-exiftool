a861d496133986f6e40c7e20aef2d609ed62e39b
Craig <craigoda@gmail.com>
2019-07-15 20:06:14 -0700

    adjusted views

--- shell/views.py
+++ shell/views.py
@@ -3,17 +3,20 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 def homepage(request):
-    image_file_name = "/home/craig/Pictures/theta/2019/z1-test-image/R0010765.JPG"
+    # pass in file name after upload for production
+    image_file_name = "/home/craig/Development/django/shell/shell/media/osaka-night.jpg"
     process = Popen(['exiftool', image_file_name], stdout=PIPE, stderr=STDOUT)
     output_byte = process.stdout.read()
     output_list = str(output_byte)[2:-1].strip().split('\\n')
-    return render(request, 'home.html', {"output": output_list})
+    return render(request, 'home.html', {"output": output_list, "filename": image_file_name.split('/')[-1]})
 
 
 def watermark(request):
+    # pass in file name after upload for production
     image_file_name = "/home/craig/Pictures/theta/2019/watermark/toyo-hardrock.jpg"
     logo_file_name = "/home/craig/Pictures/theta/2019/watermark/theta_logo.png"
     output_file = "/home/craig/Development/django/shell/shell/media/new-image.jpg"
+    # composite is part of imagemagick package
     Popen(['composite', '-geometry', '+3000+1600', logo_file_name,
         image_file_name, output_file], stdout=PIPE, stderr=STDOUT)
 
